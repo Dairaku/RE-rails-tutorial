4e0f9c71933e26dbfd700999a87168c3c7ea1327
Dairaku <dairaku051018@gmail.com>
2016-01-29 01:45:24 +0900

    第5章まで終わった

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'staticpages#home'
-  get 'staticpages/home'
+  get 'users/new'
 
-  get 'staticpages/help'
-
-   get 'staticpages/about'
-
+  root             'staticpages#home'
+  get 'help'    => 'staticpages#help'
+  get 'about'   => 'staticpages#about'
+  get 'contact' => 'staticpages#contact'
+  get 'signup'  => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
